57a0cb77110ea4af965d1ea45595159090e4314b
Phozel <adamwi@student.chalmers.se>
2022-09-20 16:27:40 +0200

    Added interface methods for IDay

--- src/Data/IDay.java
+++ src/Data/IDay.java
@@ -1,4 +1,19 @@
 package src.Data;
 
 public interface IDay {
+
+    String getDate();
+
+    String getText();
+
+    int getGrade();
+
+    IMood getMood();
+
+    ITag getTags();
+
+    void addTag(String str);
+
+    void removeTag(ITag tag);
+
 }
