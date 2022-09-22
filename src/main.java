cbdec9448cb445fc48bc6a253b8b2fb0c3aacb16
Phozel <adamwi@student.chalmers.se>
2022-09-22 22:05:55 +0200

    Impelemented IMood, and created and implemented basic functionality for the Mood class.

--- src/main.java
+++ src/main.java
@@ -2,13 +2,15 @@ package src;
 
 import src.Data.DailyPost;
 import src.Data.IDay;
+import src.Data.IMood;
+import src.Data.Mood;
 
 public class main {
 
     public static void main(String[] args){
 
         /*
-            Testing of DailyPost's methods
+        *    Testing of DailyPost's methods
         */
 
         /*IDay day = new DailyPost("1");
@@ -35,6 +37,31 @@ public class main {
         day.removeTag("Bröd");
         System.out.println(day.getTags());*/
 
+        /*
+        * Testing of Mood and IMood
+        * */
+
+        IMood anger = new Mood("Anger");
+
+        System.out.println("Initial mood name\t\t" + anger.getMoodName());
+        System.out.println("Initial mood rating\t\t" + anger.getMoodRating());
+
+        anger.changeMoodRating(50);
+
+        System.out.println("Changed mood rating\t\t" + anger.getMoodRating());
+
+        // Attempting to change moodRating to a value outside of the valid range
+
+        anger.changeMoodRating(10000);
+        anger.changeMoodRating(-90);
+
+        // Attempting to change moodRating to the lowest as well as the highest allowed value
+
+        anger.changeMoodRating(0);
+        System.out.println("Changed mood rating\t\t" + anger.getMoodRating());
+
+        anger.changeMoodRating(100);
+        System.out.println("Changed mood rating\t\t" + anger.getMoodRating());
 
     }
 }
