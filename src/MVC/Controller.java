d4b7cd5a48d6f423d62f0b2af98c724ce95f62b1
noricane <keifroch@hotmail.com>
2022-09-23 09:49:53 +0200

    tareks branch

--- src/MVC/Controller.java
+++ src/MVC/Controller.java
@@ -1,4 +1,11 @@
 package src.MVC;
 
 public class Controller {
+    //-----------------------Statistics logic start-----------------------
+    ArrayList<Integer> moodList;
+    ArrayList<Integer> dayRatingList;
+
+    LineChart lineChart;
+    PieChart pieChart;
+    //-----------------------Statistics logic end-----------------------
 }
