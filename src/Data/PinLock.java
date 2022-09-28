07a00cd8bfc0dd60340c1a9b80f24d87f8509c32
wnordlund <wilma.nordlund@gmail.com>
2022-09-28 12:06:19 +0200

    Pin Lock forts.
    Uppdaterar

--- src/Data/PinLock.java
+++ src/Data/PinLock.java
@@ -6,41 +6,39 @@ public class PinLock {
     Scanner keypad = new Scanner(System.in);
     int setPinCode = keypad.nextInt();
     int pinCode = setPinCode();
-    int getPinCode = keypad.nextInt();
     int inputPin = keypad.nextInt();
+    int getPinCode = keypad.nextInt();
 
-        public int setPinCode () {
 
+    public int setPinCode() {
         System.out.print("Enter Pin Code: ");
-
-    // int input
-
+        // int input
         return setPinCode();
     }
 
-    // onödigt att få se sin pinkod?
+        /* onödigt att få se sin pinkod?
 
         public int pinCode () {
         System.out.print("Din pinkod är: " + pinCode);
         return 0;
     }
-
-
-    public int getGetPinCode() {
-            getPinCode = this.pinCode();
-        return getPinCode;
-    }
-
-    public void setGetPinCode(int getPinCode) {
-        this.getPinCode = getPinCode;
-    }
+        */
 
     public void setPinCode(int pinCode) {
         this.pinCode = pinCode;
     }
 
-    public int inputPinCode() {
+    public int getGetPinCode() {
+            getPinCode = this.inputPinCode();
+        return getPinCode;
+    }
 
+    private int pinCode() {
+        this.pinCode = pinCode;
+        return 0;
+    }
+
+    public int inputPinCode() {
         return 0;
     }
 
