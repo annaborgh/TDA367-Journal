8ce58aabc1afab89ad31e66eab9de04a59304c14
wnordlund <wilma.nordlund@gmail.com>
2022-09-28 11:04:27 +0200

    Get, set m.m.
    Utifrån UML

--- src/Data/ILock.java
+++ src/Data/ILock.java
@@ -1,4 +1,13 @@
 package src.Data;
 
 public interface ILock {
+    boolean lockState = false;
+    boolean lockActive = false;
+    int lockId = 0;
+    boolean getLockState = false;
 }
+
+class pinLock {
+
+}
+
--- src/Data/PinLock.java
+++ src/Data/PinLock.java
@@ -1,4 +1,22 @@
 package src.Data;
 
+import java.util.Scanner;
+
 public class PinLock {
+    int attempts = 0;
+    Scanner keyboard = new Scanner(System.in);
+    int pinCode = 0;
+
+    public void setPinCode() {
+
+        System.out.print("Enter Pin Code: ");
+    }
+
+    public void getPinCode() {
+
+    }
+
+    public void inputPinCode() {
+
+    }
 }
