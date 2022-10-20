bc6fad930ae83e39bd4e560c255abdee7f9e1a2a
Phozel <adamwi@student.chalmers.se>
2022-10-20 10:05:19 +0200

    Added delete directory method. Tests are close to 100% coverage in all aspects.

--- test/src/MVC/PersistenceTest.java
+++ test/src/MVC/PersistenceTest.java
@@ -9,8 +9,12 @@ import javax.swing.filechooser.FileSystemView;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 /**
  * @author Adam Wikström
@@ -19,21 +23,32 @@ import java.util.ArrayList;
  */
 public class PersistenceTest {
 
-    Model model = new Model();
-    Persistence persistence = new Persistence();
+    private Model model = new Model();
+    private Persistence persistence = new Persistence();
+    private HashMap<LocalDate, IDay> posts = new HashMap<>();
+
 
     @Rule
     public ExpectedException exception = ExpectedException.none();
 
     /**
-     * Method to test the savePosts method in the Persistance class without exception
+     * Method to test the loadPost method in the Persistance Class
      */
     @Test
-    public void testSavePostsWithoutException(){
+    public void testLoadPosts(){
+        persistence.loadPosts();
+    }
 
+    /**
+     * Method to test the savePosts method in the Persistance class
+     */
+    @Test
+    public void testSavePosts() throws IOException {
+
+        deleteDirectory();
         // Creating a post to save
         ArrayList<ITag> testListTags = new ArrayList<>();
-        ITag testTag = new Tag("testTag", model.getAllTags().size()+1);
+        ITag testTag = new Tag("testTag", model.getAllTags().size() + 1);
         testListTags.add(testTag);
 
         ArrayList<IMood> testListMoods = new ArrayList<>();
@@ -47,62 +62,29 @@ public class PersistenceTest {
 
         // Saving the post
         persistence.savePosts(model.getPosts());
-
     }
 
-    /**
-     * Method to test the savePosts method in the Persistance class with exception
-     * @throws IOException  Exception is thrown if directory doesn't exist
-     */
-    @Test (expected = IOException.class)
-    public void testSavePostsWithException() throws IOException {
-        deleteDirectory();
-        persistence.savePosts(model.getPosts());
 
-    }
 
-    /**
-     * Method to test the loadPost method in the Persistance Class with exception
-     * @throws IOException  Exception is thrown if directory doesn't exist
-     */
-    @Test (expected = IOException.class)
-    public void testLoadPostsWithException() throws IOException {
-        deleteDirectory();
-        persistence.loadPosts();
-    }
-
-    /**
-     * Method to test the loadPost method in the Persistance Class without exception
-     */
-    @Test
-    public void testLoadPostsWithoutException(){
-        persistence.loadPosts();
-    }
-
-    File file = new File(Path.of(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separatorChar + "TDA367_Journal").toUri());
     /**
      * Method to delete the save directory of the project if it exists
      * @throws IOException  Exception is thrown if the directory to be deleted doesn't exist
      */
     private void deleteDirectory() throws IOException {
-        if (Files.isDirectory(Path.of(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separatorChar + "TDA367_Journal"))){
-            deleteFolder(file);
-        } else{
-            Files.deleteIfExists(Path.of(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separatorChar + "TDA367_Journal"));
+
+        File directory = new File(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separatorChar + "TDA367_Journal");
+        File[] templist = directory.listFiles();
+        if(templist.length == 1){
+            for (int i = 0; i < templist[0].listFiles().length; i++){
+                templist[0].listFiles()[i].delete();
+            }
+
         }
+
+        Files.delete(Path.of(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separatorChar + "TDA367_Journal" + File.separatorChar + "Posts"));
+        Files.delete(Path.of(FileSystemView.getFileSystemView().getDefaultDirectory().getPath() + File.separatorChar + "TDA367_Journal"));
+
     }
 
-    public void deleteFolder(File folder) {
-        File[] files = folder.listFiles();
-        if(files!=null) { //some JVMs return null for empty dirs
-            for(File f: files) {
-                if(f.isDirectory()) {
-                    deleteFolder(f);
-                } else {
-                    f.delete();
-                }
-            }
-        }
-        folder.delete();
-    }
+
 }
